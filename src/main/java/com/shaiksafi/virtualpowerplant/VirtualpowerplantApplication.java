b0a7a6ec9608f0432851c76a6bfa667c56c5437f
Shaik Safi <shaiksafi2001@gmail.com>
2023-12-05 13:09:50 +0530

    Initial commit

---
+++ src/main/java/com/shaiksafi/virtualpowerplant/VirtualpowerplantApplication.java
@@ -0,0 +1,13 @@
+package com.shaiksafi.virtualpowerplant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualpowerplantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualpowerplantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shaiksafi/virtualpowerplant/VirtualpowerplantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shaiksafi.virtualpowerplant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualpowerplantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
