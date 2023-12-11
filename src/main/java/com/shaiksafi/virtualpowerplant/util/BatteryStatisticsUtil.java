e5d085c7aefe29bf7f55dca9bf2263ea8da78859
Shaik Safi <shaiksafi2001@gmail.com>
2023-12-11 11:21:57 +0530

    Final commit

--- src/main/java/com/shaiksafi/virtualpowerplant/util/BatteryStatisticsUtil.java
+++ src/main/java/com/shaiksafi/virtualpowerplant/util/BatteryStatisticsUtil.java
@@ -6,17 +6,22 @@ import java.util.stream.Collectors;
 import com.shaiksafi.virtualpowerplant.dto.BatteryDto;
 import com.shaiksafi.virtualpowerplant.dto.BatteryStatisticsResponseDto;
 
+//Utility class for calculating battery statistics.
 public class BatteryStatisticsUtil {
-
+	
+	//Calculates battery statistics based on the provided list of BatteryDto objects.
 	public static BatteryStatisticsResponseDto calculateBatteryStatistics(List<BatteryDto> batteryDtos) {
+		// Calculate total watt capacity of all batteries
 		double total = batteryDtos.stream().mapToDouble(dto -> dto.wattCapacity()).sum();
+		// Calculate average watt capacity of all batteries
 		double average = batteryDtos.stream().mapToDouble(dto -> dto.wattCapacity()).average().orElse(0.0);
+		// Extract and sort battery names
 		List<String> names = batteryDtos.stream()
 		        .map(BatteryDto::name)
 		        .sorted()
 		        .collect(Collectors.toList());
 
-
+		// Create and return the BatteryStatisticsResponseDto
         return new BatteryStatisticsResponseDto(total, average, names);
     }
 }
